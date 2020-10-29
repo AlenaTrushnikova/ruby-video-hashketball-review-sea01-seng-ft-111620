db507215403afe85feb65d7fbaf7294087799d0f
Alena Trushnikova <alena.yu.t@gmail.com>
2020-10-29 21:29:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,5 +130,6 @@ end
 # Write code here
 
 def num_points_scored player_name
-  game_hash[:home][:players].merge(game_hash[:away][:players])
+  players = game_hash[:home][:players].merge(game_hash[:away][:players])
+  binding.pry
 end
